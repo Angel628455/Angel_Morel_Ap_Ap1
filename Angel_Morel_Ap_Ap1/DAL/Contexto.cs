87143974ef8fed42f9248bc39c504f832b298d58
Angel Miguel Morel Rosario <angel_morel@ucne.edu.do>
2025-02-17 18:44:20 -0500

    Creacion de Contexto

--- Angel_Morel_Ap_Ap1/DAL/Contexto.cs
+++ Angel_Morel_Ap_Ap1/DAL/Contexto.cs
@@ -1,6 +1,10 @@
-﻿namespace Angel_Morel_Ap_Ap1.Contexto
+﻿using Angel_Morel_Ap_Ap1.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Angel_Morel_Ap_Ap1.Contexto
 {
     public class Contexto
     {
+        public DbSet<RegistroAportes> registroAportes { get; set; }
     }
 }
